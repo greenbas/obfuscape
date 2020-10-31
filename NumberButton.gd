86e1eb2ffb3d0eb0b9670f9410309a9a389fc4a2
Steven Greenbank <stevengreenbank9@gmail.com>
2020-10-30 23:11:08 -0600

    code cleanup

--- NumberButton.gd
+++ NumberButton.gd
@@ -1,23 +1,25 @@
-extends Node2D
+extends Button
 
-onready var TextLabel = get_node("Button/number")
-onready var Btn = get_node("Button")
-export var isDesiredResult = false
+var correctness : bool setget set_correctness,get_correctness
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+
+func set_correctness(val : bool):
+	correctness = val;
+	
+func get_correctness() -> bool:
+	return correctness
 
 signal answer_ready(value)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	TextLabel.text = "3"
-	Btn.connect("pressed",self,"_button_pressed")
-	print(TextLabel)
+	self.connect("pressed",self,"_button_pressed")
 
 
 func _button_pressed():
-	emit_signal("answer_ready",isDesiredResult)
+	emit_signal("answer_ready",correctness)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
