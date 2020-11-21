0790cce8f39bf1aaf5c2345af43a0d548c28ffb5
Steven Greenbank <stevengreenbank9@gmail.com>
2020-11-20 22:23:49 -0600

    Revert "added crt filter"
    This reverts commit d57338a56f7ce9cc6e90182cff22b61cc1269707.

--- src/Games/Arithmetic_Game/Arithmetic_Game.gd
+++ src/Games/Arithmetic_Game/Arithmetic_Game.gd
@@ -4,16 +4,10 @@ onready var ControlContainer = $Control
 onready var QuestionLabel = get_node("Control/Question")
 onready var AnswerL = get_node("Control/AnswerL")
 onready var AnswerR = get_node("Control/AnswerR")
-export var red_box : Texture
-export var green_box : Texture
 var answer_result : bool
 
-
-
 func _init():
-
 	set_game_type(globals.game_types.ARITHMETIC)
-	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,7 +15,6 @@ func _ready():
 	AnswerL.disabled = false
 	AnswerR.disabled = false
 	var pData = GameData.prompt_data
-	var dFlag : bool = GameData.desired_flag
 	QuestionLabel.text = pData.question
 	AnswerL.set_text(str(pData.L.val)) 
 	AnswerL.set_correctness(pData.L.flag)
@@ -29,10 +22,6 @@ func _ready():
 	AnswerR.set_correctness(pData.R.flag)
 	AnswerL.connect("answer_ready",self,"_on_player_answer")
 	AnswerR.connect("answer_ready",self,"_on_player_answer")
-	if dFlag:
-		QuestionLabel.set_texture(green_box)
-	else: 
-		QuestionLabel.set_texture(red_box)
 	game_timer.start(globals.timer_start_value)
 
 
